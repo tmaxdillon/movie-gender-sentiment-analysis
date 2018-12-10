5afb2036692f6a18e33c4ef4014ce756ad6555fa
Rachel <rchlfranz@gmail.com>
2018-12-09 18:19:35 -0800

    making sentiment analyzer pep8 compliant

--- analyze_comments_tblob.py
+++ analyze_comments_tblob.py
@@ -1,16 +1,23 @@
+"""This module analyzes the sentiment of a comment usign TextBlob it converts the
+polarity score to a binary result: 1 if the polarity score is greater than or equal
+to 0 (positive) or 0 if less than 0 (negative)
+"""
+
 from textblob import TextBlob
-import pandas as pd
-from sklearn.metrics import accuracy_score
 
 def analyze_comments(sentiment_df):
-	for index, row in sentiment_df.iterrows():
-	    stripped_whitespace = row[0].replace("\r", "")
-	    sentiment_df.at[index,'comment']=stripped_whitespace
-	    comment =  sentiment_df.at[index,'comment']
-	    blob = TextBlob(comment)
+    """This function iterates through the row of a dataframe
+    and analyzes the comment at each row and adds a sentiment
+    score value next to each comment
+    """
 
-	    if blob.sentiment.polarity>=0:
-	    	sentiment_df.at[index, 'tblob_label']= '1'
-	    else:
-	    	sentiment_df.at[index, 'tblob_label']= '0'
-	return sentiment_df
+    for index, row in sentiment_df.iterrows():
+        stripped_whitespace = row[0].replace("\r", "")
+        sentiment_df.at[index, 'comment'] = stripped_whitespace
+        comment = sentiment_df.at[index, 'comment']
+        blob = TextBlob(comment)
+        if blob.sentiment.polarity >= 0:
+            sentiment_df.at[index, 'tblob_label'] = '1'
+        else:
+            sentiment_df.at[index, 'tblob_label'] = '0'
+    return sentiment_df
--- get_sentiment_score.py
+++ get_sentiment_score.py
@@ -1,4 +1,8 @@
-from urllib import request
+"""This module creates a new dataframe with a movie id and its corresponding
+mean sentiment score. Mean sentiment score is computed by taking the average of
+the sentiment scores for all the movie's comments
+"""
+
 from os import listdir
 import pandas as pd
 import numpy as np
@@ -8,30 +12,38 @@ PATH = "movie_comments"
 ANALYZED_PATH = "labeled_comments"
 
 def get_sentiment_score():
-    final_df = pd.DataFrame(columns=['movie_id','sentiment_score'])
+    """This function makes a new df with the movie_id and sentiment score
+    as columns
+    """
+
+    final_df = pd.DataFrame(columns=['movie_id', 'sentiment_score'])
     filenames2 = find_csv_filenames(PATH)
-    
     for name in filenames2:
         new_name = PATH+"/"+name
-        df = pd.read_csv(new_name, encoding ='latin1')
-        sentiment_df = pd.DataFrame(data = df)
+        df = pd.read_csv(new_name, encoding='latin1')
+        sentiment_df = pd.DataFrame(data=df)
         sentiment_df.columns = ["comment"]
-        
         if not sentiment_df.empty:
             sentiment_df = tblob.analyze_comments(sentiment_df)
-
             if name.endswith('.csv'):
                 name = name[:-4]
-            sentiment_score = np.asarray(sentiment_df.iloc[:,1], dtype=np.float).mean()
+            sentiment_score = np.asarray(sentiment_df.iloc[:, 1], dtype=np.float).mean()
             final_df = add_row(name, sentiment_score, final_df)
     # final_df.to_csv("sentiment_scores.csv", encoding='latin1')
     return final_df
 
 def find_csv_filenames(path_to_dir, suffix=".csv"):
-    filenames = listdir(path_to_dir)
-    return [ filename for filename in filenames if filename.endswith( suffix ) ]
+    """This function returns a list of all the filenames that end with '.csv'
+    """
 
-def add_row(movie_id,mean_score, final_df):
-    df2 = pd.DataFrame([[movie_id, mean_score]], columns=['movie_id','sentiment_score'])
+    filenames = listdir(path_to_dir)
+    return [filename for filename in filenames if filename.endswith(suffix)]
+
+def add_row(movie_id, mean_score, final_df):
+    """This function adds a row to the data frame with the movie_id and corresponding
+    sentiment_score values
+    """
+
+    df2 = pd.DataFrame([[movie_id, mean_score]], columns=['movie_id', 'sentiment_score'])
     final_df = final_df.append(df2, ignore_index=True)
     return final_df
