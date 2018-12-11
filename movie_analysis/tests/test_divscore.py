9a3e810d1a1eb427e4aa882c01c640658f03d5e6
Trent Dillon <tmaxd@uw.edu>
2018-12-10 16:22:27 -0800

    adding diversity score testing

---
+++ movie_analysis/tests/test_divscore.py
@@ -0,0 +1,47 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+"""This module runs unit tests over functions in diversity_score_module"""
+
+import os.path
+import unittest
+import pandas as pd
+import diversity_score_module as dsm
+
+class TestDivScore(unittest.TestCase):
+
+
+    """This class runs unit tests over functions in the diversity_score_module"""
+
+
+    # test compute_diversity_score function
+    def test_correcttype(self):
+        """This function tests if the values returned by compute_diversity_score 
+        are the correct type
+        """
+
+        [ds,cs] = dsm.compute_diversity_score()
+        self.assertTrue(type.ds == np.float64)
+        self.assertTrue(type.cs == np.float64)
+
+    def test_savefig(self):
+        """This function tests that a figure is saved when viz_distribution is run"""
+        file = 'Figures/Full_Diversity_Dist.pdf'
+        dsm.viz_distribution([1,2,3,4,5,6,7],'diversity score')
+        assert os.path.isfile(file) is True
+
+    def test_subsetrows(self):
+        """This function tests if the returned dataframe has the correct number of rows
+        """
+        
+        subsetno = 70
+        strata = 10
+        subset = dsm.get_subset(df,strata,subsetno)
+        self.assertEqual(len(subset),subsetno)
+
+SUITE = unittest.TestLoader().loadTestsFromTestCase(TestSentiment)
+_ = unittest.TextTestRunner().run(SUITE)
+
