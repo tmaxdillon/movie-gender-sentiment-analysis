7b4af7b2c88a5157f458684fe2f12ea55ac86ec3
Trent Dillon <tmaxd@uw.edu>
2018-12-11 14:27:26 -0800

    Final Commit (hopefully)

--- movie_analysis/diversity_score_module.py
+++ movie_analysis/diversity_score_module.py
@@ -1,4 +1,4 @@
-
+"""Package of functions for diversity score"""
 # coding: utf-8
 
 # In[ ]:
@@ -6,12 +6,11 @@
 
 import pandas as pd
 import numpy as np
-import json
-import matplotlib
 import matplotlib.pyplot as plt
 
 #calculate diversity score for a single movie
-def compute_diversity_score(cast,wanttoweight):
+def compute_diversity_score(cast, wanttoweight):
+    """ This function computes diversity score for a single movie"""
     castsize = len(cast) #number of cast members
     score = 0 #initialize
     if castsize == 0 or castsize == []: #no cast listed
@@ -19,65 +18,65 @@ def compute_diversity_score(cast,wanttoweight):
     else:
         if wanttoweight:
             #asymmetrical sigmoidal curve fit from https://mycurvefit.com/
-            weights = [ 0.00557557 + (1.002644 - 0.00557557)/(1 + (x/1.794024)**8.661941)**0.5556762
-               for x in np.linspace(1,10,20)] #weight lead roles more (up to 20 roles)
+            weights = [0.00557557 + (1.002644 - 0.00557557)/(1 + (x/1.794024)**8.661941)**0.5556762
+                       for x in np.linspace(1, 10, 20)] #weight lead roles more (up to 20 roles)
         else:
             weights = np.ones(20)
         if castsize > 20: #more than 20 memebrs in cast
-            ind = range(0,20)
+            ind = range(0, 20)
         else: #less than 20 members in cast
-            ind = range(0,castsize)
-        for c in ind: #loop over all cast members
-            if cast[c]['gender'] == 1: #female
-                score = score + weights[c] #female cast members boost score
-                m_all = False #debug boolean
-            elif cast[c]['gender'] != 2: #not female nor male gender number
-                weights[c] = 0; #discount entry
-        if sum(weights[:c+1]) == 0:
+            ind = range(0, castsize)
+        for cha in ind: #loop over all cast members
+            if cast[cha]['gender'] == 1: #female
+                score = score + weights[cha] #female cast members boost score
+            elif cast[cha]['gender'] != 2: #not female nor male gender number
+                weights[cha] = 0 #discount entry
+        if sum(weights[:cha+1]) == 0:
             score = np.nan
         else:
-            score = score/sum(weights[:c+1]) #take the weighted average of the score
+            score = score/sum(weights[:cha+1]) #take the weighted average of the score
     return score, castsize
 
-#visualize 
-def viz_distribution(data,type):
-    n_bins = 20 
+#visualize
+def viz_distribution(data, type):
+    """This function visualizes distribution for cast size and diversity score"""
+    n_bins = 20
     if type == 'diversity score':
-        fig, ax = plt.subplots()   
-        n, bins, patches = plt.hist(data, bins=n_bins, alpha=0.35, edgecolor='k')
-        ax.set(xlabel='gender diversity score', ylabel='number of movies',
-               title='Histogram of Gender Diversity Score')
+        fig, axe = plt.subplots()
+        plt.hist(data, bins=n_bins, alpha=0.35, edgecolor='k')
+        axe.set(xlabel='gender diversity score', ylabel='number of movies',
+                title='Histogram of Gender Diversity Score')
     elif type == 'cast size':
-        fig, ax = plt.subplots()   
+        fig, axe = plt.subplots()
         data = data[np.argwhere(~np.isnan(data))]
-        binwidth =10
+        binwidth = 10
         n_bins = range(0, int(10*np.floor(max(data)/10)) + binwidth, binwidth)
-        n, bins, patches = plt.hist(data, bins=n_bins, alpha=0.35, edgecolor='k', facecolor='g')
-        ax.set(xlabel='cast size', ylabel='number of movies',
-               title='Histogram of Cast Size')
-    ax.grid()
+        plt.hist(data, bins=n_bins, alpha=0.35, edgecolor='k', facecolor='g')
+        axe.set(xlabel='cast size', ylabel='number of movies',
+                title='Histogram of Cast Size')
+    axe.grid()
     #median and percentile values
     mean = plt.axvline(data.mean(), color='r', linestyle='dashed', linewidth=1)
-    median = plt.axvline(np.percentile(data,50), color='k', linestyle='dashed', linewidth=1)
-    ninety = plt.axvline(np.percentile(data,90), color='k', linestyle='dashed', linewidth=1)
-    ten = plt.axvline(np.percentile(data,10), color='k', linestyle='dashed', linewidth=1)
-    ax.legend((mean, median, ninety, ten), ('mean','median','90%','10%'))
+    median = plt.axvline(np.percentile(data, 50), color='k', linestyle='dashed', linewidth=1)
+    ninety = plt.axvline(np.percentile(data, 90), color='k', linestyle='dashed', linewidth=1)
+    ten = plt.axvline(np.percentile(data, 10), color='k', linestyle='dashed', linewidth=1)
+    axe.legend((mean, median, ninety, ten), ('mean', 'median', '90%', '10%'))
     if type == 'diversity score':
         fig.savefig('Figures/Full_Diversity_Dist.pdf')
     elif type == 'cast size':
         fig.savefig('Figures/Cast_Size_dist.pdf')
 
 #extract subset of data
-def get_subset(df,strata,subsetno):
-    df_subset = pd.DataFrame(columns=list(df)) #initialize
-    df_real = df.dropna() #get rid of nans
-    edges = np.linspace(0,1,strata+1) #edges of data strata
-    for i in range(0,strata):
+def get_subset(dframe, strata, subsetno):
+    """This function extracts a subset of the data"""
+    df_subset = pd.DataFrame(columns=list(dframe)) #initialize
+    df_real = dframe.dropna() #get rid of nans
+    edges = np.linspace(0, 1, strata+1) #edges of data strata
+    for i in range(0, strata):
         df_temp = df_real[(df_real['gender diversity score'] > edges[i]) &
                           (df_real['gender diversity score'] < edges[i+1])]
-        temp_ind = np.round(np.linspace(0,len(df_temp)-1,subsetno/strata))
-        df_subset = pd.concat([df_subset, 
-                          df_temp.sort_values(by=['gender diversity score']).iloc[temp_ind,:].reset_index(drop=True)], 
-                             ignore_index=True)
+        temp_ind = np.round(np.linspace(0, len(df_temp)-1, subsetno/strata))
+        df_subset = pd.concat([df_subset,
+                               df_temp.sort_values(by=['gender diversity score']).
+                               iloc[temp_ind, :].reset_index(drop=True)], ignore_index=True)
     return df_subset
-
--- movie_analysis/tests/test_divscore.py
+++ movie_analysis/tests/test_divscore.py
@@ -8,9 +8,9 @@
 
 import os.path
 import unittest
+import json
 import pandas as pd
 import numpy as np
-import json
 from movie_analysis import diversity_score_module as dsm
 
 class TestDivScore(unittest.TestCase):
@@ -21,27 +21,24 @@ class TestDivScore(unittest.TestCase):
 
     # test compute_diversity_score function
     def test_correcttype(self):
-        """This function tests if the values returned by compute_diversity_score 
+        """This function tests if the values returned by compute_diversity_score
         are the correct type
         """
-        cast = json.loads(pd.read_csv('../data/raw_data/tmdb_5000_credits.csv')['cast'][5]) #load dataset
-        [ds,cs] = dsm.compute_diversity_score(cast,True)
-        self.assertTrue(type.ds == np.float64 & type.cs == np.float64)
+        cast = json.loads(pd.read_csv('../data/raw_data/tmdb_5000_credits.csv')
+                          ['cast'][5]) #load dataset
+        [dvs, cas] = dsm.compute_diversity_score(cast, True)
+        self.assertTrue(isinstance(dvs) == np.float64 & isinstance(cas) == np.float64)
 
     def test_savefig(self):
         """This function tests that a figure is saved when viz_distribution is run"""
-        file = 'Figures/Full_Diversity_Dist.pdf'
-        dsm.viz_distribution(np.array([2,3,1,8,3,8]),'diversity score')
-        assert os.path.isfile(file) is True
+        dsm.viz_distribution(np.array([2, 3, 1, 8, 3, 8]), 'diversity score')
+        self.assertTrue(os.path.isfile('Figures/Full_Diversity_Dist.pdf'))
 
     def test_subsetrows(self):
         """This function tests if the returned dataframe has the correct number of rows
         """
-        df = pd.read_csv('../data/raw_data/tmdb_5000_credits.csv')
+        dframe = pd.read_csv('../data/raw_data/tmdb_5000_credits.csv')
         subsetno = 70
         strata = 10
-        subset = dsm.get_subset(df,strata,subsetno)
-        self.assertEqual(len(subset),subsetno)
-
-
-
+        subset = dsm.get_subset(dframe, strata, subsetno)
+        self.assertEqual(len(subset), subsetno)
