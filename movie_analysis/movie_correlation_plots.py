f99c5e66b1abd6fbae9a37c1b7776f3adbc454c0
Katherine Van Ness <kate@SUGARLOAF.localdomain>
2018-12-10 21:16:34 -0800

    were PEP8 compliant!

--- movie_analysis/movie_correlation_plots.py
+++ movie_analysis/movie_correlation_plots.py
@@ -1,11 +1,13 @@
+" Import data and produce pdf plots, including curve fitting "
+
 # Library imports:
 import pandas as pd
 import plot_functions as pf
 
-full_df = pd.read_csv('data/imdb_subset_100_10strats')
-GDS = full_df['gender diversity score']
-SS = full_df['sentiment_score']
+FULL_DF = pd.read_csv('data/imdb_subset_100_10strats')
+GDS = FULL_DF['gender diversity score']
+SS = FULL_DF['sentiment_score']
 
 # Plots:
-pf.scatter_scores(GDS,SS,'Figures/scatter.pdf')
-pf.linear_regression(GDS,SS,'Figures/linear_fit.pdf')
+pf.scatter_scores(GDS, SS, 'Figures/scatter.pdf')
+pf.linear_regression(GDS, SS, 'Figures/linear_fit.pdf')
